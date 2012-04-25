14acd2745aac6e18863335932d6565abc5e54fe7
klischsoftware <klischsoftware@fb4ba2aa-ff99-2289-8983-2dad6739c748>
2012-04-25 13:20:25 +0000

    Errors output into fail

--- trunk/Src/b.bat
+++ trunk/Src/b.bat
@@ -1,16 +1,16 @@
 @echo off
 
-U:\asm-python\tasm3\tasm -m2 -v main.asm
+tasm -m2 -v main.asm >errors.txt
 if errorlevel 1 goto exit
 
-U:\asm-python\tasm3\tasm -m2 -v util.asm
+tasm -m2 -v util.asm >errors.txt
 if errorlevel 1 goto exit
 
-U:\asm-python\tasm3\tlink -v main.obj util.obj
+tlink -v main.obj util.obj >errors.txt
 if errorlevel 1 goto exit
 
 echo Compiled successfully
 set /p x=debug?
-if %x%==y start U:\asm-python\tasm3\td main.exe
+if %x%==y start td main.exe
 
 :exit
